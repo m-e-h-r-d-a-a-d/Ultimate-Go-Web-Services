ff4a9b2514fcca71850be10460d20488b90ad6f9
mehrdad <jafari.ceai@gmail.com>
2024-02-13 12:33:49 +0330

    add kubernetes and docker

---
+++ app/services/sales-api/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
+
+	"github.com/m-e-h-r-d-a-a-d/Ultimate-Go-Web-Services/foundation/logger"
+	"go.uber.org/zap"
+)
+
+var build = "develop"
+
+func main() {
+	log, err := logger.New("SALES-API")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer log.Sync()
+
+	if err := run(log); err != nil {
+		log.Errorw("startup", "ERROR", err)
+		log.Sync()
+		os.Exit(1)
+	}
+}
+
+func run(log *zap.SugaredLogger) error {
+	// -------------------------------------------------------------------------
+	// GOMAXPROCS
+
+	log.Infow("startup", "GOMAXPROCS", runtime.GOMAXPROCS(0), "BUILD-", build)
+
+	// -------------------------------------------------------------------------\
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+
+	sig := <-shutdown
+
+	log.Infow("shutdown", "status", "shutdown started", "signal", sig)
+	defer log.Infow("shutdown", "status", "shutdown complete", "signal", sig)
+
+	return nil
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/m-e-h-r-d-a-a-d/Ultimate-Go-Web-Services
 
 go 1.22.0
+
+require go.uber.org/zap v1.26.0
+
+require go.uber.org/multierr v1.10.0 // indirect
